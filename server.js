60be58a81eb9bfef88b81a4880bb676348668c5c
postnikovpasha@mail.ru <Postnikovpasha1997>
2019-12-05 15:51:15 +0700

    change index

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/{{your-app-name}}'));
+app.use(express.static('./dist/my-appNew'));
 
 app.get('/*', function(req,res) {
 
-  res.sendFile(path.join(__dirname,'/dist/{{your-app-name}}/index.html'));
+  res.sendFile(path.join(__dirname,'/dist/my-appNew/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
