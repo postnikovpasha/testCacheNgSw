71739d0e1fdb0f38fc3bce73bd15671edae5e8f9
postnikovpasha@mail.ru <Postnikovpasha1997>
2019-12-05 16:07:37 +0700

    change index

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,5 +23,31 @@ export class AppComponent {
       alert('Ошибка /' + this.status);
     };
     XHR.send();
+
+    setTimeout(
+      () => {
+        const XHR1 = new XMLHttpRequest();
+        XHR1.responseType = 'blob';
+        XHR1.open('GET', 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/172905/test.pdf', true);
+        XHR1.onload = function() {
+          // debugger
+          const data = this.response;
+
+          const a = document.createElement('a');
+          const url = window.URL.createObjectURL(data);
+          a.href = url;
+          a.download = 'myfile.pdf';
+          document.body.append(a);
+          a.click();
+          a.remove();
+          window.URL.revokeObjectURL(url);
+        };
+        XHR1.onerror = function() {
+          console.log('Ошибка /' + this.status);
+        };
+        XHR1.send();
+      },
+      2000
+    );
   }
 }
